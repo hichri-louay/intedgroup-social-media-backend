67bc9d98d309a57a29e8b248930eea078da52ef0
hichri-louay <louay.hichri@hotmail.com>
2024-09-05 21:27:35 +0100

    add dotenv for vars

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 
-app.listen(3000, () => {  
-    console.log('Server is up and running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {  
+    console.log(`Server is up and running on port ${PORT}`);
 });
