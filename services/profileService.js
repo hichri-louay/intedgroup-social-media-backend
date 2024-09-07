b16dd3baedbf42994f5ae7c812516035821c7c87
hichri-louay <louay.hichri@hotmail.com>
2024-09-07 17:25:42 +0100

    add signed url for user picture to be accessible for every one

--- services/profileService.js
+++ services/profileService.js
@@ -1,5 +1,24 @@
 const User = require('../models/User');
+const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 
+
+const s3 = new S3Client({ 
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    }
+});
+
+const generateSignedUrl = async (key) => {
+    const command = new GetObjectCommand({
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: key
+    });
+    const signedUrl = await getSignedUrl(s3, command, { expiresIn: 604800 }); 
+    return signedUrl;
+}
 
 const changePassword = async (userId, oldPassword, newPassword) => {
     const user = await User.findById(userId)
@@ -31,7 +50,9 @@ const updateUser = async (userId, newEmail, newFirstName, newLastName, password)
 }
 
 const updateUserProfilePicture = async (userId, fileUrl) => {
-    const user = await User.findByIdAndUpdate(userId, { picture: fileUrl }, { new: true });
+    const s3Key = fileUrl.split('.amazonaws.com/')[1];
+    const signedUrl = await generateSignedUrl(s3Key);
+    const user = await User.findByIdAndUpdate(userId, { picture: signedUrl }, { new: true });
     if (!user) {
         throw new Error('User not found');
     }
